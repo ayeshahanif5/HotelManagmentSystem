2c63feb5d03489880b4d8db0ada29170401e0385
Anum Tahir <anumtahir242@gmail.com>
2018-11-21 00:52:08 -0800

    classes with controllers

---
+++ Admin/Admin/Controllers/HotelsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class HotelsController : Controller
+    {
+        // GET: Hotels
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Hotels/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Hotels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hotels/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hotels/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Hotels/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hotels/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Hotels/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Add_hotel()
+        {
+            return View();
+        }
+
+        public ActionResult Update_hotel()
+        {
+            return View();
+        }
+        public ActionResult Delete_hotel()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Admin/Admin/Controllers/Wedding_HallsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class Wedding_HallsController : Controller
+    {
+        // GET: Wedding_Halls
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Wedding_Halls/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Wedding_Halls/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Wedding_Halls/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Wedding_Halls/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Wedding_Halls/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Wedding_Halls/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Wedding_Halls/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Add_weddinghall()
+        {
+            return View();
+        }
+
+        public ActionResult Update_weddinghall()
+        {
+            return View();
+        }
+
+        public ActionResult Delete_weddinghall()
+        {
+            return View();
+        }
+
+    }
+}
